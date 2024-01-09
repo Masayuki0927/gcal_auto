541c644f9fba0be066492979e26f3a8cedf7bfff
Masayuki Ikeda <masayuki.0927.ikeda@gmail.com>
2024-01-09 21:48:34 +0900

    一旦いま動くものを入れておく

--- gcal_auto.py
+++ gcal_auto.py
@@ -5,7 +5,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sys
 
 SCOPES = [
@@ -19,6 +19,46 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/connection", methods=["post"])
+def connection():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # sync = '連携済み'
+        return '連携済みです'
+        # return redirect(url_for('index'), sync=sync)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    service = build('calendar', 'v3', credentials=creds)
+    # topic_name = 'projects/leafy-unity-409311/topics/gcal_auto' 
+    return creds
+
+# def setup_webhook():
+#     creds = connection()
+#     service = build('calendar', 'v3', credentials=creds)
+
+#     # ウェブフックの設定
+#     webhook_request = {
+#         'id': 'my-webhook-id',
+#         'address': 'https://your-webhook-endpoint.com',  # ウェブフックを受信するエンドポイントのURL
+#         'type': 'web_hook',
+#         'params': {
+#             'ttl': '3600000'  # ウェブフックの有効期限 (ミリ秒)
+#         }
+#     }
+
+#     # ウェブフックの登録
+#     service.calendarList().watch(body=webhook_request).execute()
+
 
 # Google APIに接続
 @app.route("/sync", methods=["post"])
